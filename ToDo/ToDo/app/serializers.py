90e85625071a9b0ea0a18b096cf6f4c1fe65ae5b
Filip39955 <lorkofi_aehit@students.vizja.pl>
2025-06-02 21:35:16 +0200

    Delete ToDo/ToDo directory

--- ToDo/ToDo/app/serializers.py
+++
@@ -1,9 +0,0 @@
-# app/serializers.py
-from rest_framework import serializers
-from .models import Task
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'title', 'created_at', 'user']
-        read_only_fields = ['user', 'created_at']
